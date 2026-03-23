5441c5ef6c8b875227ef2026c96d834507765231
divyabauskar <divyabauskar@gmail.com>
2026-03-23 19:55:52 +0530

    Fix GitHub pages routing blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import LanguageSelection from './components/LanguageSelection';
 import SignIn from './pages/SignIn';
 import RequestAccess from './pages/RequestAccess';
